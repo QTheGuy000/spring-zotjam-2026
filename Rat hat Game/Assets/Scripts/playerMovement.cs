a0f736309fb967392b3700f3b54713f8d81aab34
QTheGuy000 <157872344+QTheGuy000@users.noreply.github.com>
2026-04-09 21:08:56 -0700

    basicplayermovement

--- Rat hat Game/Assets/Scripts/playerMovement.cs
+++ Rat hat Game/Assets/Scripts/playerMovement.cs
@@ -2,16 +2,35 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody2D rb;
+    public float speed = 5.0f;
+    public float bounceAmount;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right * -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.right * -speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision");
+        if(collision.gameObject.CompareTag("Hat"))
+        {
+            Debug.Log("bounce");
+            rb.AddForce(Vector2.up * bounceAmount);
+        }
     }
 }
