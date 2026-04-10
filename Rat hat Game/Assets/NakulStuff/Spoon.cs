57450eb012e91b5880e5b5c5a3d18764c566c5e3
Nakul K <nakul.s.kumar@live.com>
2026-04-10 14:07:26 -0700

    Deflection

---
+++ Rat hat Game/Assets/NakulStuff/Spoon.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spoon: MonoBehaviour
+{
+    // Radius around player.
+    private float radius;
+    // Used to get mouse pos.
+    private Camera mainCamera;
+
+    private Collider2D spoonCollider;
+    private SpriteRenderer spriteRenderer;
+    private projectile touchingProjectile = null;
+    private Transform player;
+
+    private bool isSwinging = false;
+    public float swingCooldown = 0.5f;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        spoonCollider = GetComponent<Collider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        player = transform.parent;
+
+        // We can change the radius by moving where the spoon is relative from the player. 
+        radius = Vector2.Distance(transform.localPosition, Vector2.zero);
+    }
+
+    void Update()
+    {
+        RotateAroundPlayer();
+
+        // Mouse clicked.
+        if (Input.GetMouseButtonDown(0) && !isSwinging){
+            StartCoroutine(SwingSpoon());
+        }
+    }
+
+    // Rotates Spoon around player.
+    void RotateAroundPlayer()
+    { 
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPos.z = 0f;
+
+        Vector3 direction = (mouseWorldPos - player.position).normalized;
+        transform.position = player.position + direction * radius;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // Subtracted by 90 so that the spoon looks upwards.
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
+    }
+
+    // Swings the Spoon.
+    IEnumerator SwingSpoon()
+    {
+        isSwinging = true;
+        Debug.Log("Swing");
+        spriteRenderer.color = Color.yellow;
+        // Deflects Projectile (if exists).
+        if (touchingProjectile != null){
+            // Sets the direction of the bullet away from the spoon's direction. 
+            Vector2 deflectDirection = transform.up;
+            touchingProjectile.Deflect(deflectDirection);
+            Debug.Log("Deflect!");
+        }
+        // Waits before letting next swing. 
+        yield return new WaitForSeconds(swingCooldown);
+        isSwinging = false;
+        spriteRenderer.color = Color.white;
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Projectile")){
+            touchingProjectile = other.GetComponent<projectile>();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Projectile")){
+            touchingProjectile = null;
+        }
+    }
+}
--- Rat hat Game/Assets/Scripts/projectile.cs
+++ Rat hat Game/Assets/Scripts/projectile.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class projectile : MonoBehaviour
 {
-    [SerializeField] float _lifespan = 3;
+    [SerializeField] float _lifespan = 6;
     [SerializeField] float _movement_speed = 4;
     [SerializeField] float _min_movement_speed = 2;
+    private bool isDeflected = false;
 
     [SerializeField] Rigidbody2D _rigidbody;
     private playerMovement _target;
+
+    private Camera mainCamera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _target = gameController.instance.player;
         transform.right = _target.transform.position - transform.position;
+
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
@@ -26,25 +31,47 @@ public class projectile : MonoBehaviour
             Destroy(gameObject);
         }
 
+        // Destroys if it goes out of screen.
+        Vector3 screenPos = mainCamera.WorldToViewportPoint(transform.position);
+        if (screenPos.x < 0 || screenPos.x > 1 || screenPos.y < 0 || screenPos.y > 1){
+            Destroy(gameObject);
+        }
+
 
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(transform.right * _movement_speed);
-        if (_rigidbody.linearVelocity.magnitude < _min_movement_speed)
-        {
-            _rigidbody.linearVelocity = _rigidbody.linearVelocity.normalized * _min_movement_speed;
+        if (!isDeflected){
+            _rigidbody.AddForce(transform.right * _movement_speed);
+            if (_rigidbody.linearVelocity.magnitude < _min_movement_speed)
+            {
+                _rigidbody.linearVelocity = _rigidbody.linearVelocity.normalized * _min_movement_speed;
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.collider.CompareTag("Player")){
+            // Player takes damage.
+            Destroy(gameObject);
+        }
+        
     }
 
     public void setTarget(playerMovement _entity)
     {
         _target = _entity;
+    }
+
+    // Changes the direction of the bullet. 
+    public void Deflect(Vector2 newDirection){
+        transform.right = newDirection;
+        // Stops current velocity.
+        _rigidbody.linearVelocity = Vector2.zero;
+        // Adds new velocity.
+        _rigidbody.AddForce(newDirection * _movement_speed, ForceMode2D.Impulse);
+
     }
 }
