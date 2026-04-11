c8b9dba8e0728e339ea9d48f9a913174f758af2c
Nakul K <nakul.s.kumar@live.com>
2026-04-11 12:24:03 -0700

    Merged everything yay

--- Rat hat Game/Assets/NakulStuff/CloudSpawner.cs
+++ Rat hat Game/Assets/NakulStuff/CloudSpawner.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class CloudSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Cloud cloudPrefab;
+    public float spawnInterval = 6f;
+    public float cloudSpeed = 2f;
+    public float cloudLifetime = 10f;
+    public Vector3 cloudDirection = Vector3.right;
 
-    // Update is called once per frame
+    private float timer = 0f;
+
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            Cloud cloud = Instantiate(cloudPrefab, transform.position, Quaternion.identity);
+            cloud.SetDirection(cloudDirection, cloudSpeed, cloudLifetime);
+            timer = 0f;
+        }
     }
 }
