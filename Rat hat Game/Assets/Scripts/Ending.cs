da3247be2571d5085cf43dea54d119e98f153993
Kai Meng <Kaimeng44@proton.me>
2026-04-12 16:57:28 -0700

    win screens

--- Rat hat Game/Assets/Scripts/Ending.cs
+++ Rat hat Game/Assets/Scripts/Ending.cs
@@ -4,12 +4,24 @@ public class Ending : MonoBehaviour
 {
 
     [SerializeField] SpriteRenderer _sprite;
+    [SerializeField] Sprite _sprite1;
+    [SerializeField] Sprite _sprite2;
     [SerializeField] GameObject _artemis;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _sprite.enabled = false;
+
+        if (Random.Range(0, 1) ==1)
+        {
+            _sprite.sprite = _sprite1;
+        }
+        else
+        {
+            _sprite.sprite = _sprite2;
+        }
+
     }
 
     // Update is called once per frame
