87995c823ccea8e32b81c171b8a9d9d9694d096c
Kai Meng <Kaimeng44@proton.me>
2026-04-12 17:12:49 -0700

    Update CutsceneManager.cs

--- Rat hat Game/Assets/Scripts/CutsceneManager.cs
+++ Rat hat Game/Assets/Scripts/CutsceneManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CutsceneManager : MonoBehaviour
@@ -16,8 +17,10 @@ public class CutsceneManager : MonoBehaviour
     private float pause = 3f;
 
     // Start is called before the first frame update
-    void Start(){
-        foreach (Transform child in transform){
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
             frames.Add(child.gameObject);
             // Sets cutscene elements invisible
             Image image = child.gameObject.GetComponent<Image>();
@@ -30,15 +33,22 @@ public class CutsceneManager : MonoBehaviour
         StartCoroutine(FadeFrames());
     }
 
-    void Update(){
+    void Update()
+    {
         // When space is pressed, it will skip the cutscenes
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             StopAllCoroutines();
             cutsceneEnded = true;
         }
+        if (cutsceneEnded)
+        {
+            StartCoroutine(StartGame());
+        }
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         //if (!LevelManager.instance.isPaused){
         timer += Time.deltaTime;
         //}
@@ -46,15 +56,18 @@ public class CutsceneManager : MonoBehaviour
 
     // Reveals each cutscene at a time.
     // If hide is true, it will fade each image out after being shown.
-    public IEnumerator FadeFrames(bool hide = true){
-        for (int i = 0; i < frames.Count; i++){
+    public IEnumerator FadeFrames(bool hide = true)
+    {
+        for (int i = 0; i < frames.Count; i++)
+        {
             // The current cutscene being shown
             GameObject frame = frames[i];
             frame.SetActive(true);
 
             Image image = frame.GetComponent<Image>();
             // Ensures Image property exists
-            if (image){
+            if (image)
+            {
                 Color color = image.color;
                 float speed = 0.1f;
                 float fade = 0.08f;
@@ -63,7 +76,8 @@ public class CutsceneManager : MonoBehaviour
                 image.color = color;
                 // Reveals over time
                 // Increases the alpha value of the object's Image.
-                for (float a = 0f; a < 1f; a += fade){
+                for (float a = 0f; a < 1f; a += fade)
+                {
                     color.a = a;
                     image.color = color;
                     yield return new WaitForSeconds(speed);
@@ -71,7 +85,7 @@ public class CutsceneManager : MonoBehaviour
                 // Ensures it is finally revealed
                 color.a = 1f;
                 image.color = color;
-                
+
                 // Now that the scene is fully revealed, it starts showing text
                 GameObject cutsceneTextObj = frame.transform.GetChild(1).gameObject;
                 TMP_Text cutsceneText = cutsceneTextObj.GetComponent<TMP_Text>();
@@ -82,13 +96,15 @@ public class CutsceneManager : MonoBehaviour
                 // Extra time to see the screen
                 yield return new WaitForSeconds(pause);
                 // Fades out
-                if (hide){
+                if (hide)
+                {
                     // Starts revealed
                     color.a = 1f;
                     image.color = color;
                     cutsceneText.color = color;
                     // Hides over time
-                    for (float a = 1f; a > 0f; a -= fade){
+                    for (float a = 1f; a > 0f; a -= fade)
+                    {
                         color.a = a;
                         image.color = color;
                         cutsceneText.color = color;
@@ -106,13 +122,21 @@ public class CutsceneManager : MonoBehaviour
     }
 
     // Returns the current time
-    public float GetTime(){
+    public float GetTime()
+    {
         return timer;
     }
 
     // Returns a bool indicating if the cutscenes have finished
-    public bool IsCutsceneEnded(){
+    public bool IsCutsceneEnded()
+    {
         return cutsceneEnded;
+    }
+
+    public IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Level1Scene");
     }
 
 }
